97906773befc84077d9fd5a38aa628584484aeca
bhoomi-ka <85781630+bhoomi-ka@users.noreply.github.com>
2021-06-12 12:50:59 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,7 @@
+name="bhoomika"
+last_name="kashmira"
+value1=20
+value2=50.89
+print('My name is {} {}'.format(name,last_name))
+print(f'I am an integer and my value is {value1}')
+print('I am a float number and my value is {}'.format(value2))
