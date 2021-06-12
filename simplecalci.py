3d3c52fd81856f31f11ce8e2d6814418ed9c8269
bhoomi-ka <85781630+bhoomi-ka@users.noreply.github.com>
2021-06-12 13:11:17 +0530

    Add files via upload

---
+++ simplecalci.py
@@ -0,0 +1,16 @@
+num1=int(input("enter first number: "))
+num2=int(input("enter second number: "))
+add=num1+num2
+print("addition of num1 & num2:" ,add)
+sub=num1-num2
+print("subtraction of num1 & num2:" ,sub)
+mul=num1*num2
+print("multiplication of num1 & num2:" ,mul)
+fdiv=num1/num2
+print("floor division of num1 & num2:" ,fdiv)
+ddiv=num1//num2
+print("decimal division of num1 & num2:" ,ddiv)
+rem=num1%num2
+print("remainder of num1 & num2:" ,rem)
+power=num1**num2
+print("power of num1 & num2:" ,power)
