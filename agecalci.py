5f608c60e4f8dc4c3c673c62046a7dd7aed83846
bhoomi-ka <85781630+bhoomi-ka@users.noreply.github.com>
2021-06-12 12:52:58 +0530

    Add files via upload

---
+++ agecalci.py
@@ -0,0 +1,7 @@
+#the following program consider that birthday 
+#in the current year has already occured
+year=int(input("Enter the year of birth: "))
+current_year=int(input("Enter current_year: "))
+age=current_year-year
+print("calculated age:",age)
+
